f75526d045aea30988f6ee520c406ed77e2d215c
Khanilo Vyacheslav <khaniloslava@yandex.ru>
2019-05-23 14:09:22 +0300

    add export & serve static tasks

--- next.config.js
+++ next.config.js
@@ -1,19 +1,38 @@
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const withPlugins = require('next-compose-plugins');
+const bundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
 const isServerless = process.env.SERVERLESS === 'true';
 
-module.exports = withBundleAnalyzer({
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html',
-    },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../.next/bundles/client.html',
-    },
+const nextConfig = {
+  exportPathMap: async function() {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/blog': { page: '/blog' },
+    };
   },
   target: isServerless ? 'serverless' : 'server',
-});
+};
+
+module.exports = withPlugins(
+  [
+    [
+      bundleAnalyzer,
+      {
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../bundles/server.html',
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: '../.next/bundles/client.html',
+          },
+        },
+      },
+    ],
+  ],
+  nextConfig
+);
